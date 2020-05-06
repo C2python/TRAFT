54ac77bbe8ecb3ffeebde4cfb3c33d405723189b
zhangwen <wenzhangyuan@yeah.net>
2020-05-06 15:46:28 +0800

    Add Expr Recheck Feature.
    Signed-off-by: wenzhangyuan@yeah.net

---
+++ src/compat.h
@@ -0,0 +1,13 @@
+#ifndef TRAFT_COMPAT_H
+#define TRAFT_COMPAT_H
+
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression) ({     \
+  __typeof(expression) __result;              \
+  do {                                        \
+    __result = (expression);                  \
+  } while (__result == -1 && errno == EINTR); \
+  __result; })
+#endif
+
+#endif
